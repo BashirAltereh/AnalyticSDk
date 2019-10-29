27ae4ccffc55d339bc38b32a948a1363f004a67c
Bashir <bashiralterh1998@gmail.com>
2019-10-29 16:30:51 +0200

    -Get device info

--- analyticandroid/src/main/java/com/example/analyticandroid/utils/Function.java
+++ analyticandroid/src/main/java/com/example/analyticandroid/utils/Function.java
@@ -1,10 +1,23 @@
 package com.example.analyticandroid.utils;
 
+import android.os.Build;
 import android.util.Log;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Function {
-    public void openSession() {
+    public String openSession() {
         Log.d("Function", "openSession");
+        String info = "Android " + android.os.Build.VERSION.RELEASE + " \n\nDevice Model*: " + Build.DEVICE +
+                "\n\nFingerprint*:" + Build.FINGERPRINT + "\n\nPackage: " + this.getClass().getCanonicalName();
+        Map<String, String> map = new HashMap<>();
+        map.put("SystemVersion", android.os.Build.VERSION.RELEASE);
+        map.put("DeviceModel", Build.DEVICE);
+        map.put("Fingerprint", Build.FINGERPRINT);
+        map.put("Package", this.getClass().getCanonicalName());
+        Log.d("Function","Function: "+map);
+        return map.toString();
     }
 
     public void closeSession() {
