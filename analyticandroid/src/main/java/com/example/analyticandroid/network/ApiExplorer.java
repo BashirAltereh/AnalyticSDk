6c2c830ae170779137237a0c04695f1795dd4470
Bashir <bashiralterh1998@gmail.com>
2019-10-29 16:30:38 +0200

    -Make HTTP request without using external library

--- analyticandroid/src/main/java/com/example/analyticandroid/network/ApiExplorer.java
+++ analyticandroid/src/main/java/com/example/analyticandroid/network/ApiExplorer.java
@@ -1,4 +1,51 @@
 package com.example.analyticandroid.network;
 
-public class ApiExplorer {
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.os.AsyncTask;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class ApiExplorer extends AsyncTask<String, Void, Bitmap> {
+    public static void craeteRequest(Context context) {
+        HttpURLConnection connection = null;
+        Toast.makeText(context,"createRequest",Toast.LENGTH_SHORT).show();
+        try {
+            Log.d("Result_","11111");
+            connection = (HttpURLConnection) (new URL("https://api.androidhive.info/json/contacts.json")).openConnection();
+            Log.d("Result_","22222");
+            connection.setRequestMethod("GET"); // or post
+            Log.d("Result_","33333");
+            connection.connect();
+            Log.d("Result_","44444");
+            InputStream is = connection.getInputStream();
+            Log.d("Result_","Response: "+is.toString());
+        } catch (Exception e) {
+            Log.d("Result_","Error: "+e);
+        }
+    }
+
+    @Override
+    protected Bitmap doInBackground(String... strings) {
+        HttpURLConnection connection = null;
+
+        try {
+            Log.d("Result_","11111");
+            connection = (HttpURLConnection) (new URL("https://api.androidhive.info/json/contacts.json")).openConnection();
+            Log.d("Result_","22222");
+            connection.setRequestMethod("GET"); // or post
+            Log.d("Result_","33333");
+            connection.connect();
+            Log.d("Result_","44444");
+            InputStream is = connection.getInputStream();
+            Log.d("Result_","Response: "+is.toString());
+        } catch (Exception e) {
+            Log.d("Result_","Error: "+e);
+        }
+        return null;
+    }
 }
--- app/src/main/java/com/example/analyitcandroidtest/MainActivity.java
+++ app/src/main/java/com/example/analyitcandroidtest/MainActivity.java
@@ -3,12 +3,59 @@ package com.example.analyitcandroidtest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.example.analyticandroid.network.ApiExplorer;
+import com.example.analyticandroid.utils.Function;
+import com.example.analyticandroid.utils.LifeCycle;
+
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
-
+     TextView mTvInfo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mTvInfo  = findViewById(R.id.tv_info);
+        mTvInfo.setText((new Function()).openSession());
+        ApiExplorer.execute(new Runnable() {
+            @Override
+            public void run() {
+                Log.d("Result_","run...");
+
+                HttpURLConnection connection = null;
+                try {
+                    Log.d("Result_","11111");
+                    connection = (HttpURLConnection) (new URL("https://api.androidhive.info/json/contacts.json")).openConnection();
+                    Log.d("Result_","22222");
+                    connection.setRequestMethod("GET"); // or post
+                    Log.d("Result_","33333");
+                    connection.connect();
+                    Log.d("Result_","44444");
+                    InputStream is = connection.getInputStream();
+                    Log.d("Result_","Response: "+is);
+                    int i;
+                    String s = "";
+                    while((i = is.read())!=-1) {
+
+                        // converts integer to character
+                        char c = (char)i;
+
+                        // prints character
+                        s+=c;
+                        Log.d("Result_","Response: "+c);
+                    }
+                        Log.d("Result_","Response: "+s);
+
+
+                } catch (Exception e) {
+                    Log.d("Result_","Error: "+e);
+                }
+            }
+        });
     }
 }
