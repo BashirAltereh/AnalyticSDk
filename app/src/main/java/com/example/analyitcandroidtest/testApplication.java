cd92cd0213ca67655a6dbee6d9384bafe85b32d0
Bashir <bashiralterh1998@gmail.com>
2019-12-08 16:51:37 +0200

    -optimizing

---
+++ app/src/main/java/com/example/analyitcandroidtest/testApplication.java
@@ -0,0 +1,13 @@
+package com.example.analyitcandroidtest;
+
+import android.app.Application;
+
+import com.example.analyticandroid.UserTracking.SDKApplication;
+
+public class testApplication extends SDKApplication {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+}
